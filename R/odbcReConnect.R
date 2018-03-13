852403ec95ea3216bcd0926c65e0d90c8e722fcc
ds4ci <jim@ds4ci.org>
2018-03-13 12:37:04 -0700

    fix emph

--- R/odbcReConnect.R
+++ R/odbcReConnect.R
@@ -6,16 +6,15 @@
 #' Note: odbc::IsValid does not check the connection is actually still live, which
 #' is why we use dplyr::tbl() to test if live.
 #'
-#' Will throw error \emph(Could not re-establish connection.), if new connection is not
+#' Will throw error \emph{Could not re-establish connection.}, if new connection is not
 #' \code(identical()) to original.
 #'
 #' @param con An established DBI/odbc connection object
 #' @param on_table The DB table, possibly in a schema, to use for test.
 #'
 #' @return TRUE when connection is up, perhaps after re-connecting.
-#' @export
 #'
-#' @examples
+#' @export
 odbcReConnect <- function(con,
                         on_table = in_schema("gt_stg", "gt_run_parameters")){
   rt <- try(tbl(con, on_table), silent = TRUE)
